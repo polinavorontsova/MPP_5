f087dc2811c39b38216f226ea0b4da5e6056a5be
polina.vorontsova <polina.vorontsova.21@mail.ru>
2021-11-23 22:33:52 +0300

    Added instatntiation type and test classes from task description

---
+++ Tests/TestClasses/TestClasses.cs
@@ -0,0 +1,66 @@
+﻿namespace Tests.TestClasses
+{
+    internal interface ITest
+    {
+    }
+
+    internal class Test : ITest
+    {
+    }
+
+    internal class TestNested : ITest
+    {
+    }
+
+    internal interface IRepository
+    {
+    }
+
+    internal class RepositoryImpl : IRepository
+    {
+        public RepositoryImpl(ITest test)
+        {
+        }
+    }
+
+    internal interface IService<TRepository> where TRepository : IRepository
+    {
+    }
+
+    internal class ServiceImpl<TRepository> : IService<TRepository>
+        where TRepository : IRepository
+    {
+        public ServiceImpl(TRepository repository)
+        {
+            Repository = repository;
+        }
+
+        public TRepository Repository { get; }
+    }
+
+    internal interface ITestSecond
+    {
+    }
+
+    internal abstract class TestSecond : ITestSecond
+    {
+    }
+
+    internal interface IServiceWithDependency
+    {
+    }
+
+    internal class ServiceWithDependency : IServiceWithDependency
+    {
+        public ServiceWithDependency(IRepository repository)
+        {
+            Repository = repository;
+        }
+
+        public IRepository Repository { get; }
+    }
+
+    internal interface INotRegisteredInterface
+    {
+    }
+}
