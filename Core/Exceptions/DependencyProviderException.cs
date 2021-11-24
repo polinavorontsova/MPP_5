ab199b41aadd2ea69fd75931c00d1ce890966fcf
polina.vorontsova <polina.vorontsova.21@mail.ru>
2021-11-24 20:13:22 +0300

    Added exception classes for provider and configuration

---
+++ Core/Exceptions/DependencyProviderException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Core.Exceptions
+{
+    public class DependencyProviderException : Exception
+    {
+        public DependencyProviderException(string message) : base(message)
+        {
+        }
+
+        public DependencyProviderException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ Core/Exceptions/DependencyRegistrationException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Core.Exceptions
+{
+    public class DependencyRegistrationException : Exception
+    {
+        public DependencyRegistrationException(string message) : base(message)
+        {
+        }
+
+        public DependencyRegistrationException(string message, Exception innerException) : base(message,
+            innerException)
+        {
+        }
+    }
+}
